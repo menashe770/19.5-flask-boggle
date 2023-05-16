3e47f1388dfb4fe2c1fb38dac32e0deb7d936055
Menashe <mborukhov770@gmail.com>
2023-05-16 15:36:19 -0600

    step 3a

--- static/boggle.js
+++ static/boggle.js
@@ -1,7 +1,11 @@
 class BoggleGame {
-  constructor() {}
+  constructor() {
+    $('.add-word').on('submit', this.handleSubmit.bind(this));
+  }
 
   handleSubmit(e) {
     e.preventDefault();
   }
 }
+
+let game = new BoggleGame();
