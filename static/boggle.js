dd86c7fabfe132c34cf8be505689e8ca623e706f
Menashe <mborukhov770@gmail.com>
2023-05-17 14:46:18 -0600

    step 4

--- static/boggle.js
+++ static/boggle.js
@@ -3,6 +3,11 @@ class BoggleGame {
     $('.add-word').on('submit', this.handleSubmit.bind(this));
 
     this.score = 0;
+    this.words = new Set();
+  }
+
+  showScore() {
+    $('.score').text(this.score);
   }
 
   showMessage(msg) {
@@ -17,6 +22,11 @@ class BoggleGame {
     let word = $word.val();
     if (!word) return;
 
+    if (this.words.has(word)) {
+      this.showMessage(`${word} already used`);
+      return;
+    }
+
     const response = await axios.get('/check-word', { params: { word: word } });
 
     if (response.data.result === 'not-word') {
@@ -25,8 +35,12 @@ class BoggleGame {
       this.showMessage(`${word} is not on this board`);
     } else {
       this.score += word.length;
+      this.showScore();
       this.showMessage(`${word} is A-OK!`);
+      this.words.add(word);
+      $('.added').append('<li>' + word + '</li>');
     }
+    $('.add-word').trigger('reset');
 
     // this.showMessage(response.data.result);
   }
